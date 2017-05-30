71b6c5af9596986dea272c5cf16fbd258c97a557
Rafael Cruz <rafaelcruz_net@yahoo.com.br>
2017-05-30 20:45:05 -0300

    Initial Commit

---
+++ IdentityServer/src/IdentityServerSample/IdentityServerConfiguration.cs
@@ -0,0 +1,74 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityServerSample
+{
+    public class IdentityServerConfiguration
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>()
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("API", "Api Resources")
+            };
+        }
+
+        public static IEnumerable<Client> GetClientScope()
+        {
+            return new List<Client>()
+            {
+                new Client()
+                {
+                    ClientId = "79E0C2E2-D750-45BC-8FA3-1A9D5F9F82B5",
+                    ClientName = "MVC Client Access",
+                    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+
+                    ClientSecrets =
+                    {
+                        new Secret("1234567890".Sha256())
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "API"
+                    },
+                    RedirectUris = { "http://localhost:19855/signin-oidc" },
+
+                    PostLogoutRedirectUris = { "http://localhost:19855" }
+                }
+            };
+
+        }
+
+        public static IEnumerable<TestUser> GetUsers()
+        {
+            yield return new TestUser()
+            {
+                SubjectId = "AAF38B9A-4989-4B8E-B6F5-3B6928CF36C1",
+                Username = "tester_user",
+                Password = "123456789",
+                Claims = new List<Claim>()
+                {
+                    new Claim("name", "Tester User"),
+                },
+                
+            };
+        }
+    }
+}
---
+++ IdentityServer/src/MvcClientSample/Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace MvcClientSample
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationScheme = "cookies",
+            });
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions
+            {
+                AuthenticationScheme = "oidc",
+                SignInScheme = "cookies",
+
+                Authority = "http://localhost:14035",
+                PostLogoutRedirectUri = "http://localhost:19855",
+                RequireHttpsMetadata = false,
+                ClientId = "79E0C2E2-D750-45BC-8FA3-1A9D5F9F82B5",
+                ClientSecret = "1234567890",
+                ResponseType = "code id_token",
+                Scope = { "API" },
+                GetClaimsFromUserInfoEndpoint = true,
+                SaveTokens = true
+            });
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+
+        }
+    }
+}
